26f297f65168c683b6ce8f576f03da5ec741a1e5
John Neerdael <john@neerdael.nl>
2025-01-11 19:26:43 +0100

    ersion bump

--- custom_components/hyperhdr_control/__init__.py
+++ custom_components/hyperhdr_control/__init__.py
@@ -5,7 +5,6 @@ from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
 from homeassistant.const import CONF_HOST, CONF_PORT, Platform
 from homeassistant.helpers import device_registry as dr
-import asyncio
 
 from .const import DOMAIN
 
@@ -35,24 +34,13 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
         sw_version="1.3.3",
     )
 
-    # Set up all platforms
-    for platform in PLATFORMS:
-        hass.async_create_task(
-            hass.config_entries.async_forward_entry_setup(entry, platform)
-        )
-
+    # Set up all platforms using the recommended method
+    await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
     return True
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Unload a config entry."""
-    unload_ok = all(
-        await asyncio.gather(
-            *[
-                hass.config_entries.async_forward_entry_unload(entry, platform)
-                for platform in PLATFORMS
-            ]
-        )
-    )
+    unload_ok = await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
     
     if unload_ok:
         hass.data[DOMAIN].pop(entry.entry_id)
